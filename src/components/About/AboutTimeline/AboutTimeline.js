7b60a89cbf9788a867ebb17799ca333ddb4cf81c
zJuarez <mji2000.741@gmail.com>
2021-05-03 01:06:11 -0500

    implemented both timeline views

--- src/components/About/AboutTimeline/AboutTimeline.js
+++ src/components/About/AboutTimeline/AboutTimeline.js
@@ -1,8 +1,12 @@
 // @flow
 import React from 'react'
+import { VerticalTimeline } from 'react-vertical-timeline-component'
 import { Container, Row } from 'react-bootstrap'
 import HorizontalTimeline from './HorizontalTimeline'
 import './AboutTimeline.css'
+import { SMALL_WIDTH } from '../../../constants'
+import AboutSingleTimelineEvent from './AboutSingleTimelineEvent/AboutSingleTimelineEvent'
+import useWindowSize from '../../../hooks/useWindowSize'
 
 type Event = {
     date: string,
@@ -18,8 +22,24 @@ type Props = {
   years: Array<number>
  };
 
-const AboutTimeline = (props: Props) => {
+function AboutTimeline(props: Props) {
   const { events, years } = props
+  const { width } = useWindowSize()
+
+  if (width > SMALL_WIDTH) {
+    return (
+      <div className="about-timeline-container" test-id="1">
+        <Container fluid>
+          <Row className="justify-content-md-center" id="timeline-title">
+            <h1>
+              Our Story
+            </h1>
+          </Row>
+          <HorizontalTimeline events={events} years={years} />
+        </Container>
+      </div>
+    )
+  }
   return (
     <div className="about-timeline-container" test-id="1">
       <Container fluid>
@@ -28,10 +48,18 @@ const AboutTimeline = (props: Props) => {
             Our Story
           </h1>
         </Row>
-        <HorizontalTimeline events={events} years={years} />
+        <VerticalTimeline>
+          { events.map((event: Event, index: number) => (
+            <AboutSingleTimelineEvent
+              key={index}
+              event={event}
+            />
+          )) }
+        </VerticalTimeline>
+        {' '}
+
       </Container>
     </div>
   )
 }
-
 export default AboutTimeline
